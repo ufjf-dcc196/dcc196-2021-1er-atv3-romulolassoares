9f8c9e00599f2b44e10ce13956d9cc5cecba86e6
romulolassoares <romulosoares@ice.ufjf.br>
2021-06-17 16:24:30 -0300

    Função de soma

--- app/src/main/java/br/ufjf/dcc196/romulosoares/atividade3/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/romulosoares/atividade3/MainActivity.java
@@ -3,12 +3,56 @@ package br.ufjf.dcc196.romulosoares.atividade3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Define as variaveis dos elementos em tela
+    private EditText editTextValor1;
+    private EditText editTextValor2;
+    private TextView textViewResultadoFinal;
+    private Button buttonSomar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Busca os ids dos elementos em tela
+        editTextValor1 = findViewById(R.id.editTextValor1);
+        editTextValor2 = findViewById(R.id.editTextValor2);
+        textViewResultadoFinal = findViewById(R.id.textViewResultadoFinal);
+        buttonSomar = findViewById(R.id.buttonSomar);
+
+        //Quando o botão for clicado ele chama a função de somar
+        buttonSomar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                somar(v);
+            }
+        });
+
+    }
+
+    //Função para somar
+    public void somar(View view){
+        //Define as variaveis para armazenar os valores passados
+        double valor1 = 0.0;
+        double valor2 = 0.0;
+        double valorTotal;
+
+        //Valida caso o dado inserido editText seja nulo
+        try{
+            valor1 = Double.parseDouble(editTextValor1.getText().toString());
+            valor2 = Double.parseDouble(editTextValor2.getText().toString());
+        } catch (Exception ignored) {}
+
+        //Executa a soma dos valores
+        valorTotal = valor1 + valor2;
+
+        //Passa o valor obtido para a tela
+        textViewResultadoFinal.setText(Double.toString(valorTotal));
     }
 }
