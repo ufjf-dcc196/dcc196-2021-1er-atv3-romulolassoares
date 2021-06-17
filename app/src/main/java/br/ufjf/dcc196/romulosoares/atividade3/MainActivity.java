f292d7601b8e5409aeb50bf448c01d399e1c020d
romulolassoares <romulosoares@ice.ufjf.br>
2021-06-17 16:46:59 -0300

    Função de Multiplicação criada

--- app/src/main/java/br/ufjf/dcc196/romulosoares/atividade3/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/romulosoares/atividade3/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewResultadoFinal;
     private Button buttonSomar;
     private Button buttonSubtrair;
+    private Button buttonMultiplicar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         textViewResultadoFinal = findViewById(R.id.textViewResultadoFinal);
         buttonSomar = findViewById(R.id.buttonSomar);
         buttonSubtrair = findViewById(R.id.buttonSubtrair);
+        buttonMultiplicar = findViewById(R.id.buttonMultiplicar);
 
         //Quando o botão for clicado ele chama a função de somar
         buttonSomar.setOnClickListener(new View.OnClickListener() {
@@ -36,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //Quando o botão for clicado ele chama a função de somar
+        //Quando o botão for clicado ele chama a função de subtrair
         buttonSubtrair.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,6 +46,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Quando o botão for clicado ele chama a função de multiplicar
+        buttonMultiplicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                multiplicar(v);
+            }
+        });
 
     }
 
@@ -80,8 +89,28 @@ public class MainActivity extends AppCompatActivity {
             valor2 = Double.parseDouble(editTextValor2.getText().toString());
         } catch (Exception ignored) {}
 
-        //Executa a soma dos valores
+        //Executa a subtração dos valores
         valorTotal = valor1 - valor2;
+
+        //Passa o valor obtido para a tela
+        textViewResultadoFinal.setText(Double.toString(valorTotal));
+    }
+
+    //Função para subtrair
+    public void multiplicar(View view){
+        //Define as variaveis para armazenar os valores passados
+        double valor1 = 0.0;
+        double valor2 = 0.0;
+        double valorTotal;
+
+        //Valida caso o dado inserido editText seja nulo
+        try{
+            valor1 = Double.parseDouble(editTextValor1.getText().toString());
+            valor2 = Double.parseDouble(editTextValor2.getText().toString());
+        } catch (Exception ignored) {}
+
+        //Executa a multiplicação dos valores
+        valorTotal = valor1 * valor2;
 
         //Passa o valor obtido para a tela
         textViewResultadoFinal.setText(Double.toString(valorTotal));
